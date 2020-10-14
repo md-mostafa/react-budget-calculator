e7a90af5a6354ee693729d7c14b9a33120c4b259
md-mostafa <md.mostafa.akash@gmail.com>
2020-10-15 03:22:56 +0600

    expenses

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
+const initialExpenses = [
+  {id: uuidv4(), charge: 'rent', amount: 1600},
+  {id: uuidv4(), charge: 'car payment', amount: 400},
+  {id: uuidv4(), charge: 'credit card bill', amount: 1200}
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(initialExpenses);
+
+
+function App() { 
+  const [expenses, setExpenses] = useState(initialExpenses);
+  //<> </> short cut of react fragments
+
+  return <> 
+    <Alert />
+    <h1>Budget Calculator</h1>
+    <main className="App">
+      <ExpenseForm />
+      <ExpenseList expenses={expenses}/>
+    </main>
+    <h1>
+      total spending: {""} 
+      <span className="total">
+        $
+        {expenses.reduce((acc, curr) => {
+          return (acc += curr.amount);
+        }, 0)}
+      </span>
+    </h1>
+  </>;
 }
 
 export default App;
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const ExpenseForm = () => {
+    return (
+        <div>
+           hello from expense  from 
+        </div>
+    )
+};
+
+export default ExpenseForm;
