c8acb385e9f11856df7ce5ae62e98ee0a5c1d186
md-mostafa <md.mostafa.akash@gmail.com>
2020-10-15 03:51:50 +0600

    add functionality to expense form

--- src/App.js
+++ src/App.js
@@ -14,14 +14,38 @@ console.log(initialExpenses);
 
 
 function App() { 
+  //state values
+  //all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
   //<> </> short cut of react fragments
+  //single expense
+  const [charge, setCharge] = useState('');
+  //single amount
+  const [amount, setAmount] = useState('');
+  //functionality
+
+  const handleCharge = e => {
+    setCharge(e.target.value)
+  };
+  const handleAmount = e => {
+    setAmount(e.target.value)
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
   return <> 
     <Alert />
     <h1>Budget Calculator</h1>
     <main className="App">
-      <ExpenseForm />
+      <ExpenseForm 
+        charge={charge}
+        amount={amount}
+        handleCharge= {handleCharge}
+        handleAmount={handleAmount}
+        handleSubmit={handleSubmit}
+      />
       <ExpenseList expenses={expenses}/>
     </main>
     <h1>
